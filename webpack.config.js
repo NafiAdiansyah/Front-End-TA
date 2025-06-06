c19cad575c284ef177d81d214c510ae38ddd795f
NafiAdiansyah24 <nafiadiansyah24@gmail.com>
2025-06-07 03:05:02 +0700

    add CssMinimizer

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,22 @@ const path = require('path');
 const { GenerateSW } = require('workbox-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.scg.js', // cache busting
+    filename: 'bundle.[contenthash].js', // cache busting
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   mode: 'production', // ganti dari development ke production
+  optimization:{
+    minimizer:[
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
   module: {
     rules: [
       {
